ce77a03e78ac36e8afa54a4d47bb84883444a3a5
Annacantikk <mauliditaanna@gmail.com>
2023-05-25 14:05:31 +0700

    offcanvas

--- js/nav.js
+++ js/nav.js
@@ -7,19 +7,19 @@ fetch("template/nav-home.html")
     oldelem.parentNode.replaceChild(newelem, oldelem);
   });
 
-fetch("template/nav.html")
+fetch("template/footer.html")
   .then((res) => res.text())
   .then((text) => {
-    let oldelem = document.querySelector("script#replace_with_nav");
+    let oldelem = document.querySelector("script#replace_with_footer");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
   });
 
-fetch("template/footer.html")
+fetch("template/off-canvas.html")
   .then((res) => res.text())
   .then((text) => {
-    let oldelem = document.querySelector("script#replace_with_footer");
+    let oldelem = document.querySelector("script#replace_with_off-canvas");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
